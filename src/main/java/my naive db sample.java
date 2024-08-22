598ce7cd93700e77c48e1166911cb6cc408ffe3e
Antony Chiu <antony@mobb.ai>
2024-08-22 13:41:42 -0600

    Update my naive db sample.java

--- src/main/java/my naive db sample.java
+++ src/main/java/my naive db sample.java
@@ -9,11 +9,11 @@ import java.sql.Statement;
 public class SQLInjectionExample extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
         try {
-            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db");
-            String user = request.getParameter("username");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/db"); 
+            String user = request.getParameter("username"); 
 
-            String query = "SELECT * FROM users WHERE username2 = '" + user + "';";
-            Statement stmt = con.createStatement();
+            String query = "SELECT * FROM users WHERE username2 = '" + user + "';"; 
+            Statement stmt = con.createStatement(); 
 
             stmt.executeQuery(query);
         } catch (Exception e) {
